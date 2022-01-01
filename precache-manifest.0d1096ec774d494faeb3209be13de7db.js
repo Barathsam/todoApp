4d2f5bbf247d36d4013f79e08e8c29233926e08c
barath sam <barathsam555.p@gmail.com>
2022-01-01 14:54:42 +0530

    Updates

---
+++ precache-manifest.0d1096ec774d494faeb3209be13de7db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15301a10fd51ff1c80f85c6eb280f04",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "b3114a19ad74dffbd934",
+    "url": "/todoApp/static/css/main.83959067.chunk.css"
+  },
+  {
+    "revision": "8918d0af2062251e56c3",
+    "url": "/todoApp/static/js/2.fb5ada59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.fb5ada59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3114a19ad74dffbd934",
+    "url": "/todoApp/static/js/main.8663fd94.chunk.js"
+  },
+  {
+    "revision": "e912c3ce2ebb89821651",
+    "url": "/todoApp/static/js/runtime-main.1bb53725.js"
+  },
+  {
+    "revision": "4f646371b2a2501c7acd9110625993ca",
+    "url": "/todoApp/static/media/background.4f646371.jpg"
+  }
+]);
